ef34c22c1f4f1fbfc1b8dc699c9941d3b95b6bfa
Yehor Borkov <yehor.borkov@gmail.com>
2022-09-02 15:18:08 +0300

    feat: disabled plugins i don't like

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -44,6 +44,8 @@ local config = {
   },
   plugins = {
     init = {
+      ["akinsho/bufferline.nvim"] = { disable = true },
+      ["goolord/alpha-nvim"] = { disable = true},
       { "arcticicestudio/nord-vim" },
       { "tpope/vim-dadbod" },
       { "tpope/vim-surround" },
