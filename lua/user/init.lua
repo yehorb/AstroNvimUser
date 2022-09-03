78603c31d4bfbdd17e9f35846751918915d9d388
Yehor Borkov <yehor.borkov@gmail.com>
2022-09-03 11:56:49 +0300

    feat: familiar explorer mapping

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -37,6 +37,9 @@ local config = {
     [""] = {
       ["Y"] = '"*y',
     },
+    ["n"] = {
+      ["<C-e>"] = { "<cmd>Neotree toggle<cr>", desc = "Toggle Explorer" },
+    },
     ["v"] = {
       ["p"] = '"_dP',
       ["<Leader>bb"] = ":DB<Enter>",
