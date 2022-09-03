7c83c0daca5653c6c3a925ab8b14e7017c7c96f9
Yehor Borkov <yehor.borkov@gmail.com>
2022-09-03 12:38:54 +0300

    feat: explorer focus mapping

--- lua/user/mappings.lua
+++ lua/user/mappings.lua
@@ -4,6 +4,7 @@ local mappings = {
   },
   n = {
     ["<C-e>"] = { "<cmd>Neotree toggle<cr>", desc = "Toggle Explorer" },
+    ["<C-o>"] = { "<cmd>Neotree focus<cr>", desc = "Focus Explorer" },
   },
   v = {
     ["p"] = '"_dP',
