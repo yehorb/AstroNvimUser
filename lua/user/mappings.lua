493c33c170b85ee04876ca896728db99baa891ee
Yehor Borkov <yehor.borkov@gmail.com>
2022-09-03 13:05:10 +0300

    refactor: removed explorer maps

--- lua/user/mappings.lua
+++ lua/user/mappings.lua
@@ -2,10 +2,6 @@ local mappings = {
   [""] = {
     ["Y"] = '"*y',
   },
-  n = {
-    ["<C-e>"] = { "<cmd>Neotree toggle<cr>", desc = "Toggle Explorer" },
-    ["<C-o>"] = { "<cmd>Neotree focus<cr>", desc = "Focus Explorer" },
-  },
   v = {
     ["p"] = '"_dP',
     ["<Leader>bb"] = ":DB<Enter>",
