c45c291344e625c02485423b363e83bbdd4cd4f4
Yehor Borkov <yehor.borkov@gmail.com>
2022-09-04 11:55:50 +0300

    fix: changed cmp-buffer to index only visible buffers

--- lua/user/plugins/cmp.lua
+++ lua/user/plugins/cmp.lua
@@ -1,9 +1,17 @@
+local function get_bufnrs()
+  local bufs = {}
+  for _, win in ipairs(vim.api.nvim_list_wins()) do
+    bufs[vim.api.nvim_win_get_buf(win)] = true
+  end
+  return vim.tbl_keys(bufs)
+end
+
 local cmp = {
   sources = {
     {
       name = "buffer",
       option = {
-        get_bufnrs = vim.api.nvim_list_bufs,
+        get_bufnrs = get_bufnrs,
       },
     },
   },
