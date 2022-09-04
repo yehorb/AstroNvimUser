ce46428eb4f3ec731221f765557845ec0b9bf3ef
Yehor Borkov <yehor.borkov@gmail.com>
2022-09-04 21:41:01 +0300

    refactor: disabled more plugins

--- lua/user/plugins/init.lua
+++ lua/user/plugins/init.lua
@@ -1,9 +1,13 @@
-local init = {
+local plugins = {
+  ["NvChad/nvim-colorizer.lua"] = {  disable = true },
   ["akinsho/bufferline.nvim"] = { disable = true },
+  ["feline-nvim/feline.nvim"] = { disable = true },
   ["goolord/alpha-nvim"] = { disable = true },
+  ["williamboman/mason-lspconfig.nvim"] = { disable = true },
+  ["williamboman/mason.nvim"] = { disable = true },
   { "arcticicestudio/nord-vim" },
   { "tpope/vim-dadbod" },
   { "tpope/vim-surround" },
 }
 
-return init
+return plugins
