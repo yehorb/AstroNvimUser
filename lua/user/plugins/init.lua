a57d7c50b5e487fb2ee5689c04547f164b898f88
Yehor Borkov <yehor.borkov@gmail.com>
2022-09-05 13:57:48 +0300

    fix: pinned working version of which-key

--- lua/user/plugins/init.lua
+++ lua/user/plugins/init.lua
@@ -2,6 +2,7 @@ local plugins = {
   ["NvChad/nvim-colorizer.lua"] = {  disable = true },
   ["akinsho/bufferline.nvim"] = { disable = true },
   ["feline-nvim/feline.nvim"] = { disable = true },
+  ["folke/which-key.nvim"] = { commit = "9c190ea91939eba8c2d45660127e0403a5300b5a~1" },
   ["goolord/alpha-nvim"] = { disable = true },
   ["williamboman/mason-lspconfig.nvim"] = { disable = true },
   ["williamboman/mason.nvim"] = { disable = true },
