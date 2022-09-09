4b63357210c8d29a74e3cac9153a7c32704f4de1
Yehor Borkov <yehor.borkov@gmail.com>
2022-09-09 13:02:56 +0300

    refactor: plain table return for lsp.mappings

--- lua/user/lsp/mappings.lua
+++ lua/user/lsp/mappings.lua
@@ -1,14 +1,7 @@
 -- stylua: ignore
-local function mappings()
-  return {
-    n = {
-      ["<C-]>"] = { function() vim.lsp.buf.definition() end, desc = "Show the definition of current symbol" },
-      ["gh"] = { function() vim.lsp.buf.hover() end, desc = "Hover symbol details" },
-    },
-  }
-end
-
-return function(default)
-  default = vim.tbl_extend("force", default, mappings())
-  return default
-end
+return {
+  n = {
+    ["<C-]>"] = { function() vim.lsp.buf.definition() end, desc = "Show the definition of current symbol" },
+    ["gh"] = { function() vim.lsp.buf.hover() end, desc = "Hover symbol details" },
+  },
+}
