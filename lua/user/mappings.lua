4830eb1397e050c04d81331ff0c1eca2fb2ac04f
Yehor Borkov <yehor.borkov@gmail.com>
2022-09-22 14:04:29 +0300

    feat: single command for Neotree

--- lua/user/mappings.lua
+++ lua/user/mappings.lua
@@ -5,6 +5,8 @@ local mappings = {
   n = {
     ["<C-B>"] = ":buffer ",
     ["<C-P>"] = ":find ",
+    ["<Leader>e"] = { "<cmd>Neotree focus<cr>", desc = "Focus Explorer" },
+    ["<Leader>o"] = false,
   },
   v = {
     ["p"] = '"_dP',
