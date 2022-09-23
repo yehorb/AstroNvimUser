c754b912bde7d793cb16521e08a9d8e882f12582
Yehor Borkov <yehor.borkov@gmail.com>
2022-09-23 18:11:55 +0300

    fix: corrected dadbod mappings

--- lua/user/plugins/init.lua
+++ lua/user/plugins/init.lua
@@ -10,8 +10,13 @@ local plugins = {
     config = function()
       astronvim.which_key_register {
         v = {
-          ["<Leader>b"] = { name = "vim-dadbod." },
-          ["<Leader>bb"] = { "<Cmd>DB<Enter>", "Execute the database query." },
+          ["<Leader>b"] = { name = "vim-dadbod" },
+          -- <Cmd> does not work, as :DB requires [range] for what I want,
+          -- and <Cmd> does not provide range by default (as : does via '<,'>).
+          -- '<,'> can't be used with <Cmd>, as Command mode is not entered, and
+          -- markers are not updated. Using line("v") and line(".") is too much
+          -- hassle to go through.
+          ["<Leader>bb"] = { ":DB<CR>", "Execute the database query" },
         },
       }
     end,
