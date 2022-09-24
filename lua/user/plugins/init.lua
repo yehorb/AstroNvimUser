15583169be618d55741b70198d6200b2e1493b56
Yehor Borkov <yehor.borkov@gmail.com>
2022-09-24 14:56:18 +0300

    refactor: updated plugins definition

--- lua/user/plugins/init.lua
+++ lua/user/plugins/init.lua
@@ -1,30 +1,18 @@
 local plugins = {
+  -- disable
   ["akinsho/bufferline.nvim"] = { disable = true },
   ["feline-nvim/feline.nvim"] = { disable = true },
   ["goolord/alpha-nvim"] = { disable = true },
-  { "arcticicestudio/nord-vim" },
-  { "editorconfig/editorconfig-vim" },
   ["nvim-neo-tree/neo-tree.nvim"] = { disable = true },
+
+  -- tpope
+  { "tpope/vim-dadbod", setup = function() require "user.plugins.vim-dadbod" end, ft = "sql" },
   { "tpope/vim-repeat" },
-  {
-    "tpope/vim-dadbod",
-    config = function()
-      astronvim.which_key_register {
-        v = {
-          ["<Leader>b"] = { name = "vim-dadbod" },
-          -- <Cmd> does not work, as :DB requires [range] for what I want,
-          -- and <Cmd> does not provide range by default (as : does via '<,'>).
-          -- '<,'> can't be used with <Cmd>, as Command mode is not entered, and
-          -- markers are not updated. Using line("v") and line(".") is too much
-          -- hassle to go through.
-          ["<Leader>bb"] = { ":DB<CR>", "Execute the database query" },
-        },
-      }
-    end,
-    ft = "sql",
-  },
   { "tpope/vim-surround" },
   { "tpope/vim-vinegar" },
+
+  { "arcticicestudio/nord-vim" },
+  { "editorconfig/editorconfig-vim" },
 }
 
 return plugins
