a2d98f5d8d731a6dfe32376216b06c1c89ef0e20
Yehor Borkov <yehor.borkov@gmail.com>
2022-09-28 13:55:19 +0300

    refactor: better formatting handling for null-ls

--- lua/user/plugins/null-ls.lua
+++ lua/user/plugins/null-ls.lua
@@ -1,13 +1,19 @@
+local function format_before_save(bufnr)
+  vim.api.nvim_create_augroup("null-ls", { clear = true })
+  vim.api.nvim_create_autocmd("BufWritePre", {
+    group = "null-ls",
+    desc = "Format before save.",
+    buffer = bufnr,
+    callback = function()
+      vim.lsp.buf.formatting_sync()
+    end,
+  })
+end
+
 local setup = {
-  on_attach = function(client)
+  on_attach = function(client, bufnr)
     if client.resolved_capabilities.document_formatting then
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        desc = "Auto format before save",
-        pattern = "<buffer>",
-        callback = function()
-          vim.lsp.buf.formatting_sync()
-        end,
-      })
+      format_before_save(bufnr)
     end
   end,
 }
