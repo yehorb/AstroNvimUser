165393d296973a5552d65c606d49e3bc1831bf51
Yehor Borkov <yehor.borkov@gmail.com>
2022-10-11 13:59:43 +0300

    fix: Insert cmp behavior

---
+++ lua/user/plugins/cmp.lua
@@ -0,0 +1,15 @@
+local status_ok, cmp = pcall(require, "cmp")
+if not status_ok then
+  return {}
+end
+
+return {
+  mapping = {
+    ["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+    ["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+    ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+    ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+    ["<Down>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+    ["<Up>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+  }
+}
