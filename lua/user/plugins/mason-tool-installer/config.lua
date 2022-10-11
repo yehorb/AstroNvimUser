78b1e85381ea464304aebe6094430aa02222b97b
Yehor Borkov <yehor.borkov@gmail.com>
2022-10-11 21:02:32 +0300

    refactor(mason): moved custom package setup to dedicated file

--- lua/user/plugins/mason-tool-installer/config.lua
+++ lua/user/plugins/mason-tool-installer/config.lua
@@ -3,22 +3,7 @@ if not status_ok then
   return {}
 end
 
-local index
-status_ok, index = pcall(require, "mason-registry.index")
-if not status_ok then
-  return {}
-end
-
--- add custom package name to mason-registry
-index["broot"] = "user.plugins.mason-packages.broot"
-index["github-cli"] = "user.plugins.mason-packages.github-cli"
-index["lazygit"] = "user.plugins.mason-packages.lazygit"
-
 mason_tool_installer.setup {
-  ensure_installed = {
-    "broot",
-    "github-cli",
-    "lazygit",
-  },
+  ensure_installed = require "user.plugins.mason-packages",
   run_on_start = true,
 }
