d847be4a8c2edb9ba6bca58e82aef85a1f8c7850
Yehor Borkov <yehor.borkov@gmail.com>
2022-10-17 22:41:51 +0300

    feat(null-ls): checking for config file before registering linters

--- lua/user/mason-null-ls/setup_handlers.lua
+++ lua/user/mason-null-ls/setup_handlers.lua
@@ -3,6 +3,13 @@ if not status_ok then
   return {}
 end
 
+---is_in_cwd checks if the given filename is present in the current directory
+---@param filename string | string[]
+---@return boolean
+local function is_in_cwd(filename)
+  return #vim.fs.find(filename, { type = "file" }) > 0
+end
+
 return {
   mypy = function()
     local source = null_ls.builtins.diagnostics.mypy.with {
@@ -11,9 +18,15 @@ return {
     null_ls.register(source)
   end,
   selene = function()
-    local source = null_ls.builtins.diagnostics.selene.with {
-      extra_args = { "--config", string.format("%s/selene.toml", astronvim.install.config) },
-    }
-    null_ls.register(source)
+    if not is_in_cwd "selene.toml" then
+      return nil
+    end
+    null_ls.register(null_ls.builtins.diagnostics.selene)
+  end,
+  luacheck = function()
+    if not is_in_cwd ".luacheckrc" then
+      return nil
+    end
+    null_ls.register(null_ls.builtins.diagnostics.luacheck)
   end,
 }
