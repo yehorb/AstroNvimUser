e5be24ea2d4259438c96142032a8e46740977ac1
Yehor Borkov <yehor.borkov@gmail.com>
2023-01-19 11:15:07 +0200

    feat(platform): pick cmd shell if cl in scope

--- lua/user/platform/init.lua
+++ lua/user/platform/init.lua
@@ -9,7 +9,7 @@ function M.setup()
   query_ = Query.new()
   if query_:is_windows() then
     local shell
-    if os.getenv "NVIM_USE_CMD" == "1" then
+    if M.should_use_cmd() then
       shell = require "user.platform.windows.shell.cmd"
     else
       shell = require "user.platform.windows.shell.powershell"
@@ -18,6 +18,12 @@ function M.setup()
   else
     platform_ = Unix.new()
   end
+end
+
+function M.should_use_cmd()
+  local use_cmd_env = os.getenv "NVIM_USE_CMD" == "1"
+  local cl_in_scope = vim.fn.executable "cl" == 1
+  return use_cmd_env or cl_in_scope
 end
 
 function M.shell()
