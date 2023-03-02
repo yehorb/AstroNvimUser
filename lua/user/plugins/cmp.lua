bc1b8c72e949cbe551fb72b9523f518acc6aebff
Yehor Borkov <yehor.borkov@gmail.com>
2023-03-02 18:54:52 +0200

    feat(plugins): updated cmp behavior

--- lua/user/plugins/cmp.lua
+++ lua/user/plugins/cmp.lua
@@ -1,3 +1,5 @@
+local cmp = require "cmp"
+
 local function get_bufnrs()
   local bufs = {}
   for _, win in ipairs(vim.api.nvim_list_wins()) do
@@ -6,13 +8,30 @@ local function get_bufnrs()
   return vim.tbl_keys(bufs)
 end
 
-return {
-  sources = {
-    {
-      name = "buffer",
-      option = {
-        get_bufnrs = get_bufnrs,
-      },
+local sources = {
+  {
+    name = "buffer",
+    option = {
+      get_bufnrs = get_bufnrs,
     },
   },
+  { name = "nvim_lsp_signature_help" },
 }
+
+local function complete_or(options, key)
+  local existing_action = options.mapping[key]
+  options.mapping[key] = function(_)
+    if not cmp.visible() then
+      cmp.complete()
+    else
+      existing_action()
+    end
+  end
+end
+
+return function(options)
+  options.soruces = vim.list_extend(options.sources or {}, sources, 1, #sources)
+  complete_or(options, "<C-n>")
+  complete_or(options, "<C-p>")
+  return options
+end
--- lua/user/plugins/init.lua
+++ lua/user/plugins/init.lua
@@ -12,6 +12,9 @@ local plugins = {
   { "nvim-treesitter/nvim-treesitter-textobjects", after = "nvim-treesitter" },
   { "nvim-treesitter/playground", after = "nvim-treesitter" },
 
+  -- nvim-cmp
+  { "hrsh7th/cmp-nvim-lsp-signature-help", after = "nvim-cmp" },
+
   ["folke/neodev.nvim"] = require "user.plugins.neodev.plugin",
   ["j-hui/fidget.nvim"] = require "user.plugins.fidget.plugin",
   ["simrat39/rust-tools.nvim"] = require "user.plugins.rust-tools.plugin",
