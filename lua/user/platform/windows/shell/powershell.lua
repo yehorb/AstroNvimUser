37b5e310eaf8d1f2324303b13e033159f34f557b
Yehor Borkov <yehor.borkov@gmail.com>
2023-03-14 22:34:51 +0200

    fix(options): powershell options

--- lua/user/platform/windows/shell/powershell.lua
+++ lua/user/platform/windows/shell/powershell.lua
@@ -1,7 +1,7 @@
 return {
   shell = vim.fn.executable "pwsh" and "pwsh" or "powershell",
   shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
-  shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
+  shellredir = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
   shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
   shellquote = "",
   shellxquote = "",
