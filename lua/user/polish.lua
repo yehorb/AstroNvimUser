56399ec18f5a4c42bf044defea496e8799ecb65c
Yehor Borkov <yehor.borkov@gmail.com>
2023-03-17 22:10:19 +0200

    feat(options): ported ripgrep options

--- lua/user/polish.lua
+++ lua/user/polish.lua
@@ -1,3 +1,7 @@
 return function()
   require "user.autocommands"
+
+  if vim.fn.executable "rg" == 1 then
+    require "user.tools.ripgrep"
+  end
 end
---
+++ lua/user/tools/ripgrep.lua
@@ -0,0 +1,2 @@
+vim.opt.grepformat = vim.opt.grepformat + { "%f:%l:%c:%m" }
+vim.opt.grepprg = "rg --hidden --no-heading --smart-case --vimgrep"
