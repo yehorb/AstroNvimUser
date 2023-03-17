148638fb2337579492ca92a7b43ade9bb76a7d4b
Yehor Borkov <yehor.borkov@gmail.com>
2023-03-17 23:46:07 +0200

    feat(plugins): ported treesitter plugins

--- lua/user/plugins/nvim-treesitter.lua
+++ lua/user/plugins/nvim-treesitter.lua
@@ -1,5 +1,9 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  dependencies = {
+    { "nvim-treesitter/nvim-treesitter-textobjects" },
+    { "nvim-treesitter/playground" },
+  },
   opts = function(_, opts)
     opts.ensure_installed = { "c", "lua", "vim", "help" }
     require("nvim-treesitter.install").prefer_git = false
