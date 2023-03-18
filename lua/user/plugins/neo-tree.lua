903898132cc753ab8dd25aacfa9e7a88b3801b5d
Yehor Borkov <yehor.borkov@gmail.com>
2023-03-18 03:08:33 +0200

    feat(plugins): neo-tree correctly sets up alternate buffer

--- lua/user/plugins/neo-tree.lua
+++ lua/user/plugins/neo-tree.lua
@@ -1,43 +1,49 @@
-local function opendir()
-  if vim.bo.filetype ~= "neo-tree" then
-    vim.cmd.Neotree { "position=current", "reveal" }
-  end
-end
-
-local function toggle_node(state)
-  local node = state.tree:get_node()
-  if (node.type == "directory" or node:has_children()) and node:is_expanded() then
-    state.commands.toggle_node(state)
-  end
-  return node
-end
-
-local function reveal_parent(state)
-  toggle_node(state)
-  require("neo-tree.ui.renderer").focus_node(state, state.tree:get_node():get_parent_id())
-  toggle_node(state)
-end
-
 return {
   "nvim-neo-tree/neo-tree.nvim",
   keys = {
     { "<leader>e", false },
     { "<leader>o", false },
-    { "-", opendir },
+    {
+      "-",
+      function()
+        if vim.bo.filetype ~= "neo-tree" then
+          vim.cmd.Neotree { "position=current", "reveal" }
+        end
+      end,
+    },
   },
-  config = function(_, opts)
-    opts = vim.tbl_deep_extend("force", opts, {
-      filesystem = {
-        hijack_netrw_behavior = "open_current",
-        window = {
-          mappings = {
-            ["-"] = reveal_parent,
-          },
+  opts = {
+    filesystem = {
+      hijack_netrw_behavior = "open_current",
+      window = {
+        mappings = {
+          ["-"] = "parent_or_close",
         },
       },
-    })
+    },
+  },
+  config = function(_, opts)
     require("neo-tree").setup(opts)
     -- removes the "Window settings restored" message
     vim.api.nvim_del_augroup_by_name "NeoTree_BufLeave"
+    local bufenter = function(data)
+      local pattern = "neo%-tree [^ ]+ %[1%d%d%d%]"
+      if string.match(data.file, pattern) then
+        vim.w.neo_tree_alternate_nr = vim.fn.bufnr "#" ---@diagnostic disable-line: param-type-mismatch
+        vim.keymap.set("n", "<C-^>", function()
+          local alternate_nr = vim.fn.bufnr "#" ---@diagnostic disable-line: param-type-mismatch
+          if vim.w.neo_tree_alternate_nr ~= nil then
+            alternate_nr = vim.w.neo_tree_alternate_nr
+          end
+          vim.w.neo_tree_alternate_nr = nil
+          vim.cmd.buffer(alternate_nr)
+        end)
+      end
+    end
+    vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
+      group = vim.api.nvim_create_augroup("NeoTree_BufEnter", { clear = true }),
+      pattern = "neo-tree *",
+      callback = bufenter,
+    })
   end,
 }
