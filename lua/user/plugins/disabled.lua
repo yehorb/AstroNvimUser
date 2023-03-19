147deca0991f3a01b24eaba2ecf4301e0bd73e95
Yehor Borkov <yehor.borkov@gmail.com>
2023-03-19 14:33:18 +0200

    feat(plugins): re-enabled gitsigns

--- lua/user/plugins/disabled.lua
+++ lua/user/plugins/disabled.lua
@@ -2,7 +2,6 @@ return {
   { "AstroNvim/astrotheme", enabled = false },
   { "Shatur/neovim-session-manager", enabled = false },
   { "goolord/alpha-nvim", enabled = false },
-  { "lewis6991/gitsigns.nvim", enabled = false },
   { "nvim-telescope/telescope.nvim", enabled = false },
   { "rebelot/heirline.nvim", enabled = false },
   { "s1n7ax/nvim-window-picker", enabled = false },
