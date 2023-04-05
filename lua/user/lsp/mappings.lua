a7f8d4acbd9dc9b3e04e005ce381e12c17919baf
Yehor Borkov <yehor.borkov@gmail.com>
2023-04-05 17:55:36 +0300

    feat(lsp): diagnostic message with error code

--- lua/user/lsp/mappings.lua
+++ lua/user/lsp/mappings.lua
@@ -2,6 +2,16 @@ return {
   n = {
     ["<C-]>"] = { vim.lsp.buf.definition, desc = "Show the definition of current symbol" },
     ["gh"] = { vim.lsp.buf.hover, desc = "Hover symbol details" },
+    ["gl"] = {
+      function()
+        vim.diagnostic.open_float {
+          format = function(diagnostic)
+            return string.format("%s: %s", diagnostic.code, diagnostic.message)
+          end,
+        }
+      end,
+      desc = "Hover diagnostics",
+    },
   },
   i = {
     ["<C-_>"] = { vim.lsp.buf.signature_help, desc = "Show signature help" },
