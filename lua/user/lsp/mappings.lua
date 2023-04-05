ab9be2384a87109a12a4be787af18fd166f14065
Yehor Borkov <yehor.borkov@gmail.com>
2023-04-05 18:15:10 +0300

    feat(lsp): diagnostic list options

--- lua/user/lsp/mappings.lua
+++ lua/user/lsp/mappings.lua
@@ -1,6 +1,8 @@
 return {
   n = {
     ["<C-]>"] = { vim.lsp.buf.definition, desc = "Show the definition of current symbol" },
+    ["<Leader>lll"] = { vim.diagnostic.setloclist, desc = "Add buffer diagnostics to the location list." },
+    ["<Leader>llq"] = { vim.diagnostic.setqflist, desc = "Add all diagnostics to the quickfix list." },
     ["gh"] = { vim.lsp.buf.hover, desc = "Hover symbol details" },
     ["gl"] = {
       function()
