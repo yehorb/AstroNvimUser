8fbc346b70cc4533f427d5dc8356d81de8f837f2
Yehor Borkov <yehor.borkov@gmail.com>
2023-05-17 15:56:33 +0300

    refactor(ftplugin#python): using iter_matches for correct context

--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -1,7 +1,9 @@
+local M = {}
+
 --- get_root returns python tree root for a given buffer
 ---@param bufnr integer
 ---@return TSNode
-local function get_root(bufnr)
+function M.get_root(bufnr)
   local parser = vim.treesitter.get_parser(bufnr, "python", {})
   local tree = parser:parse()[1]
   return tree:root()
@@ -14,7 +16,7 @@ end
 
 --- format_sql_injections will format sql injections in a python file using sqlfluff
 ---@param bufnr integer | nil
-local function format_sql_injections(bufnr)
+function M.format_sql_injections(bufnr)
   bufnr = bufnr or vim.api.nvim_get_current_buf()
 
   if vim.bo[bufnr].filetype ~= "python" then
@@ -49,54 +51,17 @@ local function format_sql_injections(bufnr)
     end)
   end
 
-  local root = get_root(bufnr)
+  local root = M.get_root(bufnr)
 
   ---@type Change[]
   local changes = {}
   ---@type Diagnostic[]
   local diagnostics = {}
 
-  for id, node, metadata in injections:iter_captures(root, bufnr, 0, -1) do
-    if injections.captures[id] == "injection.content" and metadata["injection.language"] == "sql" then
-      local range = { node:range() }
-      local sql = vim.treesitter.get_node_text(node, bufnr)
-
-      local sqlfluff = job:new {
-        command = "sqlfluff.exe",
-        args = {
-          "fix",
-          "--nocolor",
-          "-",
-        },
-        writer = { sql },
-      }
-      ---@type string[], integer
-      local stdout, code = sqlfluff:sync()
-      ---@type string[]
-      local stderr = sqlfluff:stderr_result()
-
-      ---@type Change
-      local change = {
-        start_row = range[1] + 1,
-        end_row = range[3],
-        content = stdout,
-      }
-      table.insert(changes, 1, change)
-
-      if code ~= 0 then
-        ---@type Diagnostic
-        local diagnostic = {
-          buffer = bufnr,
-          lnum = range[1],
-          end_lnum = range[3],
-          col = range[2],
-          end_col = range[4],
-          severity = vim.diagnostic.severity.ERROR,
-          message = table.concat(stderr, "\n") .. table.concat(stdout, "\n"),
-          source = "sqlfluff",
-          code = tostring(code),
-        }
-        table.insert(diagnostics, 1, diagnostic)
+  for _, match, metadata in injections:iter_matches(root, bufnr, 0, -1) do
+    for id, node in pairs(match) do
+      if injections.captures[id] == "injection.content" and metadata["injection.language"] == "sql" then
+        M.process_capture(node, bufnr, job, changes, diagnostics)
       end
     end
   end
@@ -112,6 +77,55 @@ local function format_sql_injections(bufnr)
   end
 end
 
+--- process_capture filters given node text through the sqlfluff
+---@param node TSNode
+---@param bufnr integer
+---@param job any
+---@param changes Change[]
+---@param diagnostics Diagnostic[]
+function M.process_capture(node, bufnr, job, changes, diagnostics)
+  local range = { node:range() }
+  local sql = vim.treesitter.get_node_text(node, bufnr)
+
+  local sqlfluff = job:new {
+    command = "sqlfluff.exe",
+    args = {
+      "fix",
+      "--nocolor",
+      "-",
+    },
+    writer = { sql },
+  }
+  ---@type string[], integer
+  local stdout, code = sqlfluff:sync()
+  ---@type string[]
+  local stderr = sqlfluff:stderr_result()
+
+  ---@type Change
+  local change = {
+    start_row = range[1] + 1,
+    end_row = range[3],
+    content = stdout,
+  }
+  table.insert(changes, 1, change)
+
+  if code ~= 0 then
+    ---@type Diagnostic
+    local diagnostic = {
+      buffer = bufnr,
+      lnum = range[1],
+      end_lnum = range[3],
+      col = range[2],
+      end_col = range[4],
+      severity = vim.diagnostic.severity.ERROR,
+      message = table.concat(stderr, "\n"),
+      source = "sqlfluff",
+      code = tostring(code),
+    }
+    table.insert(diagnostics, 1, diagnostic)
+  end
+end
+
 vim.api.nvim_create_user_command("PythonFormatSqlInjections", function()
-  format_sql_injections()
+  M.format_sql_injections()
 end, {})
