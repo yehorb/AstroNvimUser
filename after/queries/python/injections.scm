3fec8b46bac21c5c8ef89ad182a0be1ab04ab31a
Yehor Borkov <yehor.borkov@gmail.com>
2023-05-17 15:42:02 +0300

    feat(queries#python): toml injection

--- after/queries/python/injections.scm
+++ after/queries/python/injections.scm
@@ -5,3 +5,10 @@
     string_content: (string_content) @injection.content)
   (#set! injection.language "sql")
   (#contains? @_id "sql"))
+
+(assignment
+  left: (identifier) @_id
+  right: (string
+    string_content: (string_content) @injection.content)
+  (#set! injection.language "toml")
+  (#contains? @_id "toml"))
