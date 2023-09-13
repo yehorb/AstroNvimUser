ae96e2c559e1b5e51f50c9bbf0576fff7777b25c
Yehor Borkov <yehor.borkov@gmail.com>
2023-09-13 12:36:20 +0300

    fix(queries): correct injection content capture

--- after/queries/python/injections.scm
+++ after/queries/python/injections.scm
@@ -1,7 +1,6 @@
 ; extends
 (assignment
   left: (identifier) @_id
-  right: (string
-    string_content: (string_content) @injection.content)
-  (#set! injection.language "sql")
-  (#contains? @_id "sql"))
+  right: (string (string_content) @injection.content)
+  (#contains? @_id "sql")
+  (#set! injection.language "sql"))
